ccdd347f11f9cf0c9d565cde3b1243b7c3174887
Chris Denedios <cdenedios@TherapeuticsMD.com>
2018-09-04 11:51:25 -0400

    address controller implemented

--- src/PocNetCoreDataEncryption.Api/Controllers/AddressController.cs
+++ src/PocNetCoreDataEncryption.Api/Controllers/AddressController.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PocNetCoreDataEncryption.DAL;
+using PocNetCoreDataEncryption.Domain;
 
 namespace PocNetCoreDataEncryption.Api.Controllers
 {
@@ -11,36 +11,60 @@ namespace PocNetCoreDataEncryption.Api.Controllers
     [ApiController]
     public class AddressController : ControllerBase
     {
+        private readonly IAddressRepository _addressRepository;
+
+        public AddressController(IAddressRepository addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+
         // GET: api/Address
         [HttpGet]
-        public IEnumerable<string> GetAddresses()
+        public async Task<IActionResult> GetAddresses()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var addresses = await _addressRepository.GetAsync(take: 10);
+                return Ok(addresses.ToArray());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
         // GET: api/Address/5
         [HttpGet("{id}", Name = "GetAddress")]
-        public string GetAddress(int id)
+        public async Task<IActionResult> GetAddress(int id)
         {
-            return "value";
+            try
+            {
+                var address = await _addressRepository.GetByIdAsync(id);
+                return Ok(address);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
         // POST: api/Address
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] Address address)
         {
+            try
+            {
+                _addressRepository.AddOrUpdate(address);
+                await _addressRepository.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
 
-        // PUT: api/Address/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
